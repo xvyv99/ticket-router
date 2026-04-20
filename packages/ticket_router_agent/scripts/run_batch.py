8dddf790d51dcda43eb16859603a04fd47e95687
xvyv99 <xvyv99@163.com>
2026-04-20 19:01:25 +0800

    WIP 11

---
+++ packages/ticket_router_agent/scripts/run_batch.py
@@ -0,0 +1,73 @@
+from argparse import ArgumentParser
+from logging import getLogger, basicConfig
+
+from ticket_router_base.data.loader import load_test_set
+from ticket_router_base.config import LOGGING_FORMAT, OUTPUT_DIR
+from ticket_router_base.utils import write_pred
+
+from ticket_router_agent.config import MODEL_CHOICES
+from ticket_router_agent.infer import vLLMPredictor
+from ticket_router_agent.utils import save_prefix_from_model_choice
+
+logger = getLogger(__name__)
+
+
+def run_infer(sample_num: int, model_choice: str, few_shot: bool):
+    df_test = load_test_set().head(sample_num)
+
+    predictor = vLLMPredictor(model_name_or_path=model_choice, few_shot=few_shot)
+
+    llm_batch = predictor.predict(df_test)
+
+    save_prefix = save_prefix_from_model_choice(model_choice)
+    save_path = (
+        OUTPUT_DIR
+        / "goal_based"
+        / f"{save_prefix}_{'few_shot' if few_shot else 'zero_shot'}_predictions.jsonl"
+    )
+
+    write_pred(
+        llm_batch.predictions,
+        df_test,
+        save_path,
+    )
+
+
+def main():
+    parser = ArgumentParser(description="Local vLLM inference")
+    parser.add_argument(
+        "model_choice",
+        nargs="+",
+        choices=MODEL_CHOICES,
+        help="Model choices to run",
+    )
+    parser.add_argument(
+        "--few-shot",
+        dest="few_shot",
+        action="store_true",
+        default=True,
+        help="Enable few-shot prompting (default: true)",
+    )
+    parser.add_argument(
+        "--sample-num",
+        type=int,
+        default=1200,
+        help="Number of samples",
+    )
+    args = parser.parse_args()
+
+    logger.info(
+        f"Running inference with model choices: {args.model_choice}, few_shot={args.few_shot}, sample_num={args.sample_num}"
+    )
+
+    for model_choice in args.model_choice:
+        run_infer(
+            sample_num=args.sample_num,
+            model_choice=model_choice,
+            few_shot=args.few_shot,
+        )
+
+
+if __name__ == "__main__":
+    basicConfig(level="INFO", format=LOGGING_FORMAT)
+    main()
